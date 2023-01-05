15e1babc240efa038bb1aab8619998a0bdcdc300
IceDynamix <me@icedynamix.moe>
2023-01-05 14:30:08 +0100

    Create tournament factory and seed

--- app/Models/Tournament.php
+++ app/Models/Tournament.php
@@ -3,10 +3,13 @@
 namespace App\Models;
 
 use App\Http\Enums\TetrioRank;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Tournament extends Model
 {
+    use HasFactory;
+    
     protected $casts = [
         'lower_reg_rank_cap' => TetrioRank::class,
         'upper_reg_rank_cap' => TetrioRank::class,
---
+++ database/factories/TournamentFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Http\Enums\TetrioRank;
+use App\Http\Enums\TournamentStatus;
+use App\Models\Tournament;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Carbon;
+
+class TournamentFactory extends Factory
+{
+    protected $model = Tournament::class;
+
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name(),
+            'bracket_url' => $this->faker->url(),
+            'status' => TournamentStatus::Upcoming,
+            'hidden' => false,
+            'description' => $this->faker->text(),
+            'reg_open_ts' => Carbon::now(),
+            'reg_close_ts' => Carbon::now(),
+            'check_in_open_ts' => Carbon::now(),
+            'check_in_close_ts' => Carbon::now(),
+            'tournament_start_ts' => Carbon::now(),
+            'lower_reg_rank_cap' => TetrioRank::Unranked,
+            'upper_reg_rank_cap' => TetrioRank::X,
+            'grace_rank_cap' => TetrioRank::X,
+        ];
+    }
+}
