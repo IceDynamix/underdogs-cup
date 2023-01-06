eb73cd14b7a88331adf9dd28606530a9fbdc543c
IceDynamix <me@icedynamix.moe>
2023-01-07 00:03:25 +0100

    Add rank images for registration page

--- resources/views/tournaments/register.blade.php
+++ resources/views/tournaments/register.blade.php
@@ -22,10 +22,9 @@
                             <li><a href="{{route('connect')}}">TETR.IO account linked</a></li>
                             <li>
                                 Not have placed in the top 3 of a previous Underdogs Cup before (or been blacklisted for
-                                other
-                                reasons)
+                                other reasons)
                             </li>
-                            <li>Ranked at tournament announcement</li>
+                            <li>Had a rank at tournament announcement</li>
                             @if($tournament->min_games_played)
                                 <li>Min. {{$tournament->min_games_played}} played ranked games at tournament
                                     announcement
@@ -35,19 +34,45 @@
                                 <li>Max. {{$tournament->max_rd}} rating deviation at tournament announcement</li>
                             @endif
                             @if($tournament->lower_reg_rank_cap)
-                                <li>At least {{$tournament->lower_reg_rank_cap->format()}} rank at tournament
-                                    announcement
+                                <li>
+                                    <span class="icon-text">
+                                        <span>At least</span>
+                                        <span class="icon">
+                                            <img src="{{$tournament->lower_reg_rank_cap->img()}}" alt="Lower rank cap">
+                                        </span>
+                                        <span>rank at tournament announcement</span>
+                                    </span>
                                 </li>
                             @endif
                             @if($tournament->upper_reg_rank_cap)
-                                <li>At most {{$tournament->upper_reg_rank_cap->format()}} rank at tournament
-                                    announcement
+                                <li>
+                                    <span class="icon-text">
+                                        <span>At most</span>
+                                        <span class="icon">
+                                            <img src="{{$tournament->upper_reg_rank_cap->img()}}" alt="Upper rank cap">
+                                        </span>
+                                        <span>rank at tournament announcement</span>
+                                    </span>
                                 </li>
                             @endif
                             @if($tournament->grace_rank_cap)
-                                <li>Must have never been {{$tournament->grace_rank_cap->format()}} before</li>
-                                <li>Reach at most {{$tournament->grace_rank_cap->format()}} rank until tournament
-                                    start
+                                <li>
+                                    <span class="icon-text">
+                                        <span>Must have never been</span>
+                                        <span class="icon">
+                                            <img src="{{$tournament->grace_rank_cap->img()}}" alt="Grace rank">
+                                        </span>
+                                        <span>rank before</span>
+                                    </span>
+                                </li>
+                                <li>
+                                    <span class="icon-text">
+                                        <span>Reach at most</span>
+                                        <span class="icon">
+                                            <img src="{{$tournament->grace_rank_cap->img()}}" alt="Grace rank">
+                                        </span>
+                                        <span>rank until tournament start</span>
+                                    </span>
                                 </li>
                             @endif
                         </ul>
