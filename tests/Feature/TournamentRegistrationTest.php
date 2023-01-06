89c190992543ccbaf4c48e24a4f82e9fd5f9918b
IceDynamix <me@icedynamix.moe>
2023-01-06 17:45:04 +0100

    Merge registration test classes

--- tests/Feature/TournamentRegistrationTest.php
+++ tests/Feature/TournamentRegistrationTest.php
@@ -2,8 +2,11 @@
 
 namespace Tests\Feature;
 
+use App\Enums\TetrioRank;
 use App\Enums\TournamentStatus;
+use App\Helper\RegistrationHelper;
 use App\Models\TetrioUser;
+use App\Models\TetrioUserSnapshot;
 use App\Models\Tournament;
 use App\Models\User;
 use Database\Factories\TournamentFactory;
@@ -22,20 +25,14 @@ class TournamentRegistrationTest extends TestCase
         $user = $this->okUser()->create();
         $tour = $this->tournament()->create(['status' => TournamentStatus::RegOpen]);
 
-        $this->get(route('tournaments.register', $tour))
-            ->assertRedirectToRoute('login');
-
         $this->actingAs($user)
-            ->post(route('tournaments.register.post', $tour))
+            ->get(route('tournaments.register', $tour))
             ->assertOk();
 
         $tour->update(['status' => TournamentStatus::RegClosed]);
 
-        $this->get(route('tournaments.register', $tour))
-            ->assertForbidden();
-
         $this->actingAs($user)
-            ->post(route('tournaments.register.post', $tour))
+            ->get(route('tournaments.register', $tour))
             ->assertForbidden();
     }
 
@@ -65,6 +62,59 @@ class TournamentRegistrationTest extends TestCase
             ->assertForbidden();
     }
 
+    public function testOk()
+    {
+        $tournament = $this->tournament()->create();
+        $user = $this->okUser()->create();
+        $this->snapshot($user->tetrio, $tournament);
+
+        self::assertEmpty(RegistrationHelper::getRegistrationErrors($tournament, $user));
+    }
+
+    public function testNoSnapshot()
+    {
+        $tournament = $this->tournament()->create();
+        $user = $this->okUser()->create();
+
+        self::assertNotEmpty(RegistrationHelper::getRegistrationErrors($tournament, $user));
+    }
+
+    public function testCurrentRankLow()
+    {
+        $tournament = $this->tournament()->create(['lower_reg_rank_cap' => TetrioRank::S]);
+        $user = $this->okUser(['rank' => TetrioRank::A])->create();
+        $this->snapshot($user->tetrio, $tournament);
+
+        self::assertNotEmpty(RegistrationHelper::getRegistrationErrors($tournament, $user));
+    }
+
+    public function testCurrentRankHigh()
+    {
+        $tournament = $this->tournament()->create(['upper_reg_rank_cap' => TetrioRank::S]);
+        $user = $this->okUser(['rank' => TetrioRank::SPlus])->create();
+        $this->snapshot($user->tetrio, $tournament);
+
+        self::assertNotEmpty(RegistrationHelper::getRegistrationErrors($tournament, $user));
+    }
+
+    public function testRegistrationRankLow()
+    {
+        $tournament = $this->tournament()->create(['lower_reg_rank_cap' => TetrioRank::S]);
+        $user = $this->okUser(['rank' => TetrioRank::S])->create();
+        $this->snapshot($user->tetrio, $tournament, ['rank' => TetrioRank::A]);
+
+        self::assertNotEmpty(RegistrationHelper::getRegistrationErrors($tournament, $user));
+    }
+
+    public function testRegistrationRankHigh()
+    {
+        $tournament = $this->tournament()->create(['upper_reg_rank_cap' => TetrioRank::S]);
+        $user = $this->okUser(['rank' => TetrioRank::S])->create();
+        $this->snapshot($user->tetrio, $tournament, ['rank' => TetrioRank::SPlus]);
+
+        self::assertNotEmpty(RegistrationHelper::getRegistrationErrors($tournament, $user));
+    }
+
     protected function setUp(): void
     {
         parent::setUp();
@@ -77,11 +127,29 @@ class TournamentRegistrationTest extends TestCase
         return Tournament::factory()->state(['status' => TournamentStatus::RegOpen]);
     }
 
-    private function okUser(): UserFactory
+    private function okUser(array $tetrioAttrs = []): UserFactory
     {
         return User::factory()->state([
             'is_blacklisted' => false,
-            'tetrio_user_id' => TetrioUser::factory()->create()->id,
+            'tetrio_user_id' => TetrioUser::factory()->create($tetrioAttrs)->id,
         ]);
+    }
+
+    private function snapshot(TetrioUser $tetrio, Tournament $tournament, array $attrs = []): TetrioUserSnapshot
+    {
+        $base = [
+            'user_id' => $attrs->id ?? $tetrio->id,
+            'tournament_id' => $tournament->id,
+            'rank' => $attrs->rank ?? $tetrio->rank,
+            'best_rank' => $attrs->best_rank ?? $tetrio->best_rank,
+            'rating' => $attrs->rating ?? $tetrio->rating,
+            'rd' => $attrs->rd ?? $tetrio->rd,
+            'pps' => $attrs->pps ?? $tetrio->pps,
+            'apm' => $attrs->apm ?? $tetrio->apm,
+            'vs' => $attrs->vs ?? $tetrio->vs,
+            'games_played' => $attrs->games_played ?? $tetrio->games_played,
+        ];
+
+        return TetrioUserSnapshot::create(array_replace($base, $attrs));
     }
 }
