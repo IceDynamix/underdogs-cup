91c81936f8b2bbd3ec0fa0d925cdc13679ae8b57
IceDynamix <me@icedynamix.moe>
2023-01-07 17:30:50 +0100

    readme and deploy

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/sh
+
+/usr/bin/git pull
+
+/usr/bin/composer install --no-dev
+
+/usr/bin/php artisan migrate --force
+
+npm run build
+
+# /usr/bin/php artisan cache:clear
+/usr/bin/php artisan config:clear
+/usr/bin/php artisan view:clear
+/usr/bin/php artisan route:clear
+
+/usr/bin/php artisan optimize:clear
+/usr/bin/php artisan package:discover
+/usr/bin/php artisan view:cache
+/usr/bin/php artisan route:cache
+/usr/bin/php artisan config:cache
+/usr/bin/php artisan storage:link
