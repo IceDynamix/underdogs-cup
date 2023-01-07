8b8f718b4891b44b69fb944b117970d45d04fb61
IceDynamix <me@icedynamix.moe>
2023-01-07 15:03:24 +0100

    bot: move to src level

--- bot/index.js
+++ bot/index.js
@@ -1,5 +1,7 @@
 const {Client, Events, GatewayIntentBits} = require('discord.js');
-const {token} = require('../config.json');
+require('dotenv').config();
+
+const token = process.env.DISCORD_TOKEN;
 
 const client = new Client({intents: [GatewayIntentBits.Guilds]});
 
