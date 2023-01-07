b4eddbdec8aa0b642bd3866e805f307baa440e76
IceDynamix <me@icedynamix.moe>
2023-01-07 18:19:56 +0100

    Show tag when tournament is hidden

--- resources/views/components/tournament/card.blade.php
+++ resources/views/components/tournament/card.blade.php
@@ -4,6 +4,9 @@
     <div class="tags">
         <span class="tag {{$tournament->status->cssClass()}}">{{$tournament->status->proper()}}</span>
         <span class="tag is-info">{{$tournament->rankRange()}}</span>
+        @if($tournament->hidden)
+            <span class="tag is-warning">Hidden</span>
+        @endif
     </div>
 
     <p>{{$tournament->description}}</p>
