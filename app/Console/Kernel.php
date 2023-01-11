1cf7e8ca1dff58f7cf34fd4befeaa3da0e32a224
IceDynamix <22303902+IceDynamix@users.noreply.github.com>
2023-01-11 19:47:02 +0100

    Schedule users update job

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,6 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        $schedule->command('users:update')->everySixHours();
         $schedule->command('tournament:check-unregister')->hourly();
     }
 
