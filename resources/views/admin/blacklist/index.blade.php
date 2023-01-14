7a29cde0344d0d17b4617e2994ae081276b12d9f
IceDynamix <me@icedynamix.moe>
2023-01-15 00:18:08 +0100

    Add blacklists table view

--- resources/views/admin/blacklist/index.blade.php
+++ resources/views/admin/blacklist/index.blade.php
@@ -3,13 +3,41 @@
         <div class="container">
             <div class="content">
                 <h1>Player Blacklist</h1>
-                @forelse($blacklistEntries as $entry)
-                    <p>
-                        {{$entry->user->username}}
-                    </p>
-                @empty
-                    <p>No entries in blacklist yet</p>
-                @endforelse
+                <table>
+                    <thead>
+                    <tr>
+                        <th>User</th>
+                        <th>Blacklisted</th>
+                        <th>Expires</th>
+                        <th></th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @forelse($blacklistEntries as $entry)
+                        <tr>
+                            <td>{{$entry->user->username}}</td>
+                            <td>{{$entry->created_at->diffForHumans()}}, {{$entry->created_at}}</td>
+                            <td>
+                                @if($entry->until)
+                                    {{$entry->until?->diffForHumans()}}, {{$entry->until}}
+                                @else
+                                    -
+                                @endif
+                            </td>
+                            <td>
+                                <button class="button is-danger">Delete</button>
+                            </td>
+                        </tr>
+                    @empty
+                        <tr>
+                            <td>No entries in blacklist yet</td>
+                        </tr>
+                    @endforelse
+                    </tbody>
+                </table>
+                @can('create', $entry)
+                    <button class="button is-success">Add</button>
+                @endcan
             </div>
         </div>
     </section>
